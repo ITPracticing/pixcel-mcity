2124d67dd42ab55804581f30883f8961e15fe585
testUser <testUser@email.com>
2021-03-10 18:29:41 +0800

    inital pixel-mcity project

---
+++ pixel-mcity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pixel-mcity
+//
+//  Created by F_OS on 3/10/21.
+//  Copyright © 2021 F_OS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
